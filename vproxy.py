431395bd02cfe244717daa7cf4fd875bf616304a
B4rD4k <eranvak91@gmail.com>
2016-11-28 10:43:13 +0200

    Update vproxy.py

--- vproxy.py
+++ vproxy.py
@@ -50,6 +50,7 @@ def write_conf(proxyhost,proxyport,ports,isr):
 	subprocess.Popen("bash vproxy.sh", shell=True, stdout=subprocess.PIPE).stdout.read()
 
 def cleanup():
+	print colored("[+]",'green')+colored(" Starting Cleanup, GoodBye :)","blue", attrs=['bold'])
 	subprocess.Popen("service pptpd stop", shell=True, stdout=subprocess.PIPE).stdout.read()
 	subprocess.Popen("ifconfig ppp0 down", shell=True, stdout=subprocess.PIPE).stdout.read()
 	subprocess.Popen("iptables -t nat -F", shell=True, stdout=subprocess.PIPE).stdout.read()
@@ -83,45 +84,57 @@ def main():
 	parser.add_argument('-port', help='Which port to forward to proxy, e.g 80,443', required=False)
 	args = parser.parse_args()
 
-	print colored("Starting Vproxy...", "green")
 	if (args.phost and args.pport and not args.port):
-		print colored("[!] missing port argument", "red")
+		print colored("[!] Missing port argument", "red",attrs=['bold'])
 	elif ((args.phost and not args.pport) or (args.pport and not args.phost)):
-		print colored("[!] missing proxy arguments check again","red")
+		print colored("[!] Missing proxy arguments check again","red",attrs=['bold'])
 	elif (args.phost and args.pport and args.localip and args.port):
-		print colored("[V]Configuring PPTP server and redirect HTTP/S traffic on port: ","green")+colored(str(args.port.split(",")),"yellow")+colored(" to proxy: ","green")+colored(args.phost+":"+args.pport,"yellow")
+		# print colored("[V]Starting Vproxy to forward traffic on port: ","green",attrs=['bold'])+colored(str(args.port.split(",")),"yellow",attrs=['bold'])+colored(" to proxy instance: ","green",attrs=['bold'])+colored(args.phost+":"+args.pport,"yellow",attrs=['bold'])
+		print colored("Vproxy Configurations:","cyan", attrs=['bold'])
+		print colored("=================================================","cyan")
+		print colored("[+]","green")+" Forwarding Ports: "+colored(str(args.port.split(",")),"green",attrs=['bold'])
+		print colored("[+]","green")+" Proxy host: "+colored(str(args.phost)+":"+str(args.pport),"green",attrs=['bold'])
+		print colored("=================================================","cyan")
 		write_conf(args.phost,args.pport,args.port.split(","),isr=True)
-		print "================================================="
-		print "Vproxy Output: "
-		print "1.Export and install Proxy SSL Certificate"
-		print "2.Configure new VPN profile:"
-		print "	PPTP Server: "+ colored(args.localip,"yellow")
-		print "	PPTP User: "+ colored("Vproxy","yellow")
-		print "	PPTP Password: "+ colored("Vproxy123","yellow")
-		print "3. Analzye HTTP/S traffic using your favorite proxy software"
-		print "================================================="
+		print ""
+		print colored("Vproxy Output: ","cyan", attrs=['bold'])
+		print colored("=================================================","cyan")
+		print colored('[+]','green',attrs=['bold'])+" Export and install Proxy SSL Certificate"
+		print colored('[+]','green',attrs=['bold'])+" Configure new VPN profile:"
+		print "	PPTP Server: "+ colored(args.localip,"green",attrs=['bold'])
+		print "	PPTP User: "+ colored("Vproxy","green",attrs=['bold'])
+		print "	PPTP Password: "+ colored("Vproxy123","green",attrs=['bold'])
+		print colored('[+]','green',attrs=['bold'])+" FireUp proxy and "+colored("Happy Hacking!","green",attrs=['underline','bold'])
+		print colored("=================================================","cyan")
 
 		try:
-			raw_input("[!]Press any key to close Vproxy...")
+			print ""
+			raw_input("[!]Press any key to stop Vproxy...")
+			print ""
 		except KeyboardInterrupt:
+			print "\r\n"
 			cleanup()
 
 		cleanup()
 	elif (not args.phost and not args.pport and not args.port):
-		print colored("[V]Configuring PPTP server without proxy redirection ...","green")
+		print colored("[V] Starting Vproxy...","green",attrs=['bold'])
 		write_conf(args.phost,args.pport,None,isr=False)
-		print "================================================="
-		print "Vproxy Output: "
-		print "1.Configure new VPN profile:"
-		print "	PPTP Server: "+ colored(args.localip,"yellow")
-		print "	PPTP User: "+ colored("Vproxy","yellow")
-		print "	PPTP Password: "+ colored("Vproxy123","yellow")
-		print "2. Use Wireshark or TCPDump to analyze the traffic"
-		print "================================================="
+		print ""
+		print colored("Vproxy Output: ","cyan",attrs=['bold'])
+		print colored("=================================================","cyan")
+		print colored('[+]','green',attrs=['bold'])+" Configure new VPN profile:"
+		print "	PPTP Server: "+ colored(args.localip,"green", attrs=['bold'])
+		print "	PPTP User: "+ colored("Vproxy","green",attrs=['bold'])
+		print "	PPTP Password: "+ colored("Vproxy123","green",attrs=['bold'])
+		print colored('[+]','green',attrs=['bold'])+" Use Wireshark or TCPDump to analyze the traffic"
+		print colored("=================================================","cyan")
 		
 		try:
+			print 
 			raw_input("[!]Press any key to close Vproxy...")
+			print ""
 		except KeyboardInterrupt:
+			print "\r\n"
 			cleanup()
 
 		cleanup()
